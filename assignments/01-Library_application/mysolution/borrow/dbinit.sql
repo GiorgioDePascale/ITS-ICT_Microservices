7c079d43f3dab0c1d48b7354d3c4b745d2d79dc6
GiorgioDePascale <59685505+GiorgioDePascale@users.noreply.github.com>
2021-01-29 11:05:08 +0100

    EEEE

---
+++ assignments/01-Library_application/mysolution/borrow/dbinit.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE "borrowing" OWNER postgres;
+\connect borrow
+ALTER DATABASE "borrow" SET TIMEZONE TO 'Europe/Rome';
+SET TIMEZONE TO 'Europe/Rome';
+
+CREATE TABLE "borrows"
+(
+    borrow_id integer,
+    start_borrow character varying,
+    end_borrow character varying,
+    book integer,
+    customer integer,
+    notify character varying
+) TABLESPACE pg_default;
+
+ALTER TABLE "borrow"
+    OWNER to postgres;
